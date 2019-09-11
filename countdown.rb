193c429166188a9f5f3cf2295394a500e30a8145
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-11 01:18:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
 def countdown_with_sleep
-  sleep(1)
+  sleep(5)
 end
 
 def countdown(integer)
+  countdown_with_sleep
   while integer != 0 
     puts "#{integer} SECOND(S)!"
-    countdown_with_sleep
     integer -= 1
   end
   return "HAPPY NEW YEAR!"
